c856db9ab490fad0e5cc94cf84015c697c45a90e
Rahul21-dev <125725593+Rahul21-dev@users.noreply.github.com>
2025-04-14 23:27:16 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import SSCCGLTracker from './components/SSCCGLTracker'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <SSCCGLTracker />
+    </div>
+  )
+}
+
+export default App
