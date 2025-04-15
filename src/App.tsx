2a75d89abcc745df8fb0548b7e1286711f83dba5
Rahul21-dev <125725593+Rahul21-dev@users.noreply.github.com>
2025-04-15 06:21:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import SSCCGLTracker from './components/SSCCGLTracker';
+import SSCCGLTracker from './components/SSCCGLTracker'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-function App() {
+export default function App() {
   return (
-    <div className="app">
-      <SSCCGLTracker />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1 container mx-auto px-4 py-8">
+        <SSCCGLTracker />
+      </main>
+      <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
