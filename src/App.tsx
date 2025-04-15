b65dc21c187bd2ffca9097af9650e1ba25e4eeb6
Rahul21-dev <125725593+Rahul21-dev@users.noreply.github.com>
2025-04-15 06:55:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
-      <main className="flex-1 container mx-auto px-4 py-8">
+      <main className="flex-1 p-4 md:p-8">
         <SSCCGLTracker />
       </main>
       <Footer />
