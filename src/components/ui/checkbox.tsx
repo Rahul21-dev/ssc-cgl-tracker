100f5c9f24b99a679cbfd836662cd9b2b698d6d3
Rahul21-dev <125725593+Rahul21-dev@users.noreply.github.com>
2025-04-15 08:16:25 +0530

    Update checkbox.tsx

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -5,25 +5,20 @@ import { cn } from "@/lib/utils"
 const Checkbox = React.forwardRef<
   HTMLInputElement,
   React.InputHTMLAttributes<HTMLInputElement>
->(({ className, ...props }, ref) => {
-  return (
-    <div className="relative flex items-center">
-      <input
-        type="checkbox"
-        className={cn(
-          "peer h-5 w-5 shrink-0 rounded-sm border border-gray-300",
-          "focus:outline-none focus:ring-2 focus:ring-indigo-500",
-          "disabled:cursor-not-allowed disabled:opacity-50",
-          "data-[state=checked]:bg-indigo-600 data-[state=checked]:border-indigo-600",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-      <Check className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-3.5 w-3.5 text-white opacity-0 peer-data-[state=checked]:opacity-100 pointer-events-none" />
-    </div>
-  )
-})
+>(({ className, ...props }, ref) => (
+  <div className="flex items-center">
+    <input
+      type="checkbox"
+      className={cn(
+        "h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500",
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+    <Check className="ml-2 h-4 w-4" />
+  </div>
+))
 Checkbox.displayName = "Checkbox"
 
 export { Checkbox }
