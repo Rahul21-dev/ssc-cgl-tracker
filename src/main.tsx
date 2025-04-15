74f83868ea07d9d02186292fc329032858e4c4ec
Rahul21-dev <125725593+Rahul21-dev@users.noreply.github.com>
2025-04-15 06:56:35 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { ErrorBoundary } from 'react-error-boundary'
-
-function ErrorFallback({ error }: { error: Error }) {
-  return (
-    <div className="p-4 text-red-500">
-      <h2>Something went wrong</h2>
-      <pre>{error.message}</pre>
-    </div>
-  )
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>
 )
